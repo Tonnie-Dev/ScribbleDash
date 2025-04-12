f6bdcf945daa9100e67de48333306fab4806e157
Tonnie <vontonnie@gmail.com>
2025-04-12 18:22:47 +0300

    Run refreshVersions

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -18,6 +18,10 @@ dependencyResolutionManagement {
         mavenCentral()
     }
 }
+plugins {
+    // See https://jmfayard.github.io/refreshVersions
+    id("de.fayard.refreshVersions") version "0.60.5"
+}
 
 rootProject.name = "ScribbleDash"
 include(":app")
