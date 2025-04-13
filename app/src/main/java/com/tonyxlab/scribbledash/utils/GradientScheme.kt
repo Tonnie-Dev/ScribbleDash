c861c077d38e73b14c6e25b18e022bc51cf3b177
Tonnie <vontonnie@gmail.com>
2025-04-13 11:08:22 +0300

    Create GradientScheme.kt

---
+++ app/src/main/java/com/tonyxlab/scribbledash/utils/GradientScheme.kt
@@ -0,0 +1,22 @@
+package com.tonyxlab.scribbledash.utils
+
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.ReadOnlyComposable
+import androidx.compose.runtime.staticCompositionLocalOf
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+
+object GradientScheme {
+    val backgroundGradient = Brush.sweepGradient(
+            colors = listOf(Color(0xFFFEFAF6), Color(0xFFFFF1E2)),
+            center = Offset(.5f, .5f)
+    )
+}
+
+val LocalGradient = staticCompositionLocalOf { GradientScheme }
+
+val MaterialTheme.gradient
+    @Composable @ReadOnlyComposable
+    get() = LocalGradient.current
