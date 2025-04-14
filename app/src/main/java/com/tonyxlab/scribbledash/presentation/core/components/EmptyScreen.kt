04af6d2bfdd6dd5ab63f3a3ab904d1af89e38f6f
Tonnie <vontonnie@gmail.com>
2025-04-14 10:57:34 +0300

    Add Empty Screen

---
+++ app/src/main/java/com/tonyxlab/scribbledash/presentation/core/components/EmptyScreen.kt
@@ -0,0 +1,53 @@
+package com.tonyxlab.scribbledash.presentation.core.components
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.PreviewLightDark
+import com.tonyxlab.scribbledash.R
+import com.tonyxlab.scribbledash.presentation.core.utils.GradientScheme
+import com.tonyxlab.scribbledash.presentation.theme.ScribbleDashTheme
+
+@Composable
+fun EmptyScreen(modifier: Modifier = Modifier) {
+
+    Scaffold(containerColor = MaterialTheme.colorScheme.surface) {
+        innerPadding ->
+
+        Column(
+                modifier = Modifier
+                        .fillMaxSize()
+                        .drawBehind{
+                            drawRect(brush = GradientScheme.backgroundGradient)
+                        }
+                        .padding(innerPadding),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center
+        ) {
+
+            AppHeadlineText(text = stringResource(R.string.text_empty))
+
+        }
+    }
+
+
+}
+
+
+@PreviewLightDark
+@Composable
+private fun EmptyScreenPreview() {
+    ScribbleDashTheme{
+
+        EmptyScreen()
+    }
+}
