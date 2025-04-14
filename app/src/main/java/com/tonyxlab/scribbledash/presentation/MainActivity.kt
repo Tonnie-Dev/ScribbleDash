0b7ddf80a2dc0d1b3e981b525c881ec0c3226b16
Tonnie <vontonnie@gmail.com>
2025-04-14 14:16:17 +0300

    Update MainActivity.kt

--- app/src/main/java/com/tonyxlab/scribbledash/presentation/MainActivity.kt
+++ app/src/main/java/com/tonyxlab/scribbledash/presentation/MainActivity.kt
@@ -6,42 +6,44 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavHost
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.rememberNavController
+import com.tonyxlab.scribbledash.navigation.Destinations
+import com.tonyxlab.scribbledash.navigation.Destinations.*
+import com.tonyxlab.scribbledash.navigation.appDestinations
+import com.tonyxlab.scribbledash.presentation.core.utils.spacing
 import com.tonyxlab.scribbledash.presentation.theme.ScribbleDashTheme
 
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         enableEdgeToEdge()
+
         setContent {
             ScribbleDashTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                            name = "Android",
-                            modifier = Modifier.padding(innerPadding)
+                val navController = rememberNavController()
+                val padding = MaterialTheme.spacing.spaceDefault
+
+                NavHost(
+                        navController = navController,
+                        startDestination = HomeScreenDestination
+                ) {
+
+                    appDestinations(
+                            modifier = Modifier.padding(padding),
+                            navController = navController
                     )
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-            text = "Hello $name!",
-            modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ScribbleDashTheme {
-        Greeting("Android")
     }
 }
