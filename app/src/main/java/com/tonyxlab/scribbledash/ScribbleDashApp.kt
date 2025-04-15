f49a47cf5482ea7473476970117460eb6a887cbe
Tonnie <vontonnie@gmail.com>
2025-04-15 21:43:35 +0300

    Update ScribbleDashApp.kt

--- app/src/main/java/com/tonyxlab/scribbledash/ScribbleDashApp.kt
+++ app/src/main/java/com/tonyxlab/scribbledash/ScribbleDashApp.kt
@@ -1,12 +1,22 @@
 package com.tonyxlab.scribbledash
 
 import android.app.Application
+import com.tonyxlab.scribbledash.di.appModule
+import org.koin.android.ext.koin.androidContext
+import org.koin.core.context.startKoin
 import timber.log.Timber
 
 class ScribbleDashApp : Application() {
 
     override fun onCreate() {
         super.onCreate()
+
         Timber.plant(Timber.DebugTree())
+
+        startKoin {
+            androidContext(this@ScribbleDashApp)
+            modules(appModule)
+
+        }
     }
 }
