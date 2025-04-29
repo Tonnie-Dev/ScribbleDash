aea6cf27a1ef36ed505df6a49dcfaeccadd2eac4
Tonnie <vontonnie@gmail.com>
2025-04-29 08:39:34 +0300

    Update DrawUiState.kt

--- app/src/main/java/com/tonyxlab/scribbledash/presentation/screens/draw/handling/DrawUiState.kt
+++ app/src/main/java/com/tonyxlab/scribbledash/presentation/screens/draw/handling/DrawUiState.kt
@@ -3,20 +3,28 @@ package com.tonyxlab.scribbledash.presentation.screens.draw.handling
 import androidx.compose.runtime.Stable
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
+import com.tonyxlab.scribbledash.data.ColorSerializer
+import com.tonyxlab.scribbledash.data.ListOffsetSerializer
+import kotlinx.serialization.Serializable
 
 @Stable
 data class DrawUiState(
     val selectedColor: Color = Color.Black,
     val currentPath: PathData? = null,
     val paths: List<PathData> = emptyList(),
+    val currentSvgPath: RandomVectorData = RandomVectorData(),
     val buttonsState: ButtonsState = ButtonsState(),
     val undoStack: List<PathData> = emptyList(),
     val remainingSecs: Int = 0
 ) {
+
+    @Serializable
     @Stable
     data class PathData(
         val id: Long = 0L,
+        @Serializable(with = ColorSerializer::class)
         val color: Color = Color.Black,
+        @Serializable(with = ListOffsetSerializer::class)
         val path: List<Offset> = emptyList()
     )
 
@@ -24,7 +32,14 @@ data class DrawUiState(
     data class ButtonsState(
         val undoButtonEnabled: Boolean = false,
         val redoButtonEnabled: Boolean = false,
-        val clearButtonEnabled: Boolean = false
+        val submitButtonEnabled: Boolean = false
+    )
+
+    @Stable
+    data class RandomVectorData(
+        val paths: List<String> = emptyList(),
+        val viewportWidth: Float = 0f,
+        val viewportHeight: Float = 0f
     )
 
 
