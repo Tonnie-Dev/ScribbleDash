072ab904dfeadd92fddef5f36a59727c1ee7c764
Tonnie <vontonnie@gmail.com>
2025-04-29 08:39:54 +0300

    Update AppModule.kt

--- app/src/main/java/com/tonyxlab/scribbledash/di/AppModule.kt
+++ app/src/main/java/com/tonyxlab/scribbledash/di/AppModule.kt
@@ -1,9 +1,80 @@
 package com.tonyxlab.scribbledash.di
 
+import android.R.attr.viewportHeight
+import android.R.attr.viewportWidth
+import android.app.Application
+import android.content.Context
 import com.tonyxlab.scribbledash.presentation.screens.draw.DrawViewModel
+import com.tonyxlab.scribbledash.presentation.screens.draw.handling.DrawUiState.RandomVectorData
+import com.tonyxlab.scribbledash.presentation.screens.preview.PreviewViewModel
+import com.tonyxlab.utils.getRawVectorPathData
+import org.koin.android.ext.koin.androidContext
 import org.koin.core.module.dsl.viewModel
+import org.koin.core.module.dsl.viewModelOf
 import org.koin.dsl.module
 
 val appModule = module {
-    viewModel { DrawViewModel() }
+    // Provide the required function type for DrawViewModel
+    single<(Context) -> RandomVectorData> {
+        { context: Context ->
+            val allVectors = getRawVectorPathData(context)
+            if (allVectors.isEmpty()) {
+                RandomVectorData(emptyList(), 0f, 0f)
+            } else {
+                val (_, vectorData) = allVectors.entries.random()
+                val (paths, vpWidth, vpHeight) = vectorData
+                RandomVectorData(paths, vpWidth, vpHeight)
+            }
+        }
+    }
+
+    viewModel { DrawViewModel(get(), get()) }
+    viewModelOf(::PreviewViewModel)
+    /*
+        viewModel { DrawViewModel() }
+        viewModel { PreviewViewModel() }
+
+
+
+
+    single { androidContext() }  */
 }
+
+
+val randomVectorProviderModule = module {
+    single<() -> RandomVectorData> {
+        val context = get<Context>()
+        val vectorDataMap = getRawVectorPathData(context)
+
+        if (vectorDataMap.isEmpty()) {
+            return@single {
+                RandomVectorData(emptyList(), 0f, 0f)
+            }
+        }
+
+        val randomEntry = vectorDataMap.entries.random()
+        val (paths, viewportWidth, viewportHeight) = randomEntry.value
+
+        return@single {
+            RandomVectorData(paths, viewportWidth.toFloat(), viewportHeight.toFloat())
+        }
+    }
+}
+/*val randomVectorProviderModule = module {
+    single<(Context) -> RandomVectorData> {
+        { context ->
+            val allVectors = getRawVectorPathData(context)
+            if (allVectors.isEmpty()) {
+                RandomVectorData(emptyList(), 0f, 0f)
+            } else {
+                val (_, vectorData) = allVectors.entries.random()
+                val (paths, vpWidth, vpHeight) = vectorData
+                RandomVectorData(paths, vpWidth, vpHeight)
+            }
+        }
+    }
+}*/
+
+
+
+
