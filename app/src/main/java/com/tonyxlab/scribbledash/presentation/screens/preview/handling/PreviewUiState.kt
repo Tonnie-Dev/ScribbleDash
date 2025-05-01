7d01503a2c513dcd5c38be52b2e61b049062210e
Tonnie <vontonnie@gmail.com>
2025-05-01 21:29:12 +0300

    Update PreviewUiState.kt

--- app/src/main/java/com/tonyxlab/scribbledash/presentation/screens/preview/handling/PreviewUiState.kt
+++ app/src/main/java/com/tonyxlab/scribbledash/presentation/screens/preview/handling/PreviewUiState.kt
@@ -2,6 +2,10 @@ package com.tonyxlab.scribbledash.presentation.screens.preview.handling
 
 import com.tonyxlab.scribbledash.presentation.screens.draw.handling.DrawUiState
 
-data class PreviewUiState( val score: Int,
-                           val sampleSvgData: List<String>,
-                           val pathData: List<DrawUiState.PathData>,)
+data class PreviewUiState(
+    val score: Int = 0,
+    val sampleSvgStrings: List<String> = emptyList(),
+    val userPathStrings: List<String> = emptyList(),
+    val viewPortWidth: Float = 0f,
+    val viewPortHeight: Float = 0f
+)
