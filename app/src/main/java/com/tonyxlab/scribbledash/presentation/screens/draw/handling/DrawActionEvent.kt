ad82178118a8f7e49ff13339d822fefbae1ef327
Tonnie <vontonnie@gmail.com>
2025-05-01 21:29:23 +0300

    Create DrawActionEvent.kt

---
+++ app/src/main/java/com/tonyxlab/scribbledash/presentation/screens/draw/handling/DrawActionEvent.kt
@@ -0,0 +1,14 @@
+package com.tonyxlab.scribbledash.presentation.screens.draw.handling
+
+sealed interface DrawActionEvent {
+
+    data class NavigateToPreviewScreen(
+
+        val sampleSvgPathData: List<String>,
+        val userPathData: List<String>,
+        val viewPortWidth: Float,
+        val viewPortHeight: Float
+
+    ): DrawActionEvent
+
+}
