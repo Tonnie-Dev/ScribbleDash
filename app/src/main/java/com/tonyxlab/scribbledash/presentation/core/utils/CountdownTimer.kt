ca0378f968f2272588c4af7085e12f77838be84d
Tonnie <vontonnie@gmail.com>
2025-05-24 09:41:17 +0300

    Update Countdown timer

--- app/src/main/java/com/tonyxlab/scribbledash/presentation/core/utils/CountdownTimer.kt
+++ app/src/main/java/com/tonyxlab/scribbledash/presentation/core/utils/CountdownTimer.kt
@@ -6,7 +6,6 @@ import kotlinx.coroutines.Job
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
-import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 
 class CountdownTimer(private val totalSeconds: Int) {
@@ -43,8 +42,6 @@ class CountdownTimer(private val totalSeconds: Int) {
         elapsedSeconds = 0
         _remainingSeconds.value = totalSeconds
     }
-
-
 
     private fun launchTimer() {
         job = CoroutineScope(Dispatchers.Main).launch {
